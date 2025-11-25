b6cc3585c41951cd35b640b0d7077c48513e98b3
o1mirza <o1mirza@torontomu.ca>
2025-11-24 23:38:35 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,20 +4,20 @@
 #include <imgui.h>
 #include <imgui-SFML.h>
 #include <vector>
-#include <optional>//
 #include <cmath>
 #include <memory>
 #include <string>
 #include <map>
 #include <algorithm>
+#include <iostream>
 
 // GLOBAL VALUES
 // Constant config values
 const unsigned int FPS_LOCK {60}, WIDTH {1280}, HEIGHT {720};
-const double CAMERA_SPEED {2};
+const double CAMERA_SPEED {2.0}, TIME_INTERVAL{0.1};
 
 // GLOBAL VARIABLES
-bool stop_time {false};
+bool stop_time {true}, is_solved {false}, follow_projectile {false};
 std::string user_error_message {};
 
 // Main rendering surface 
@@ -32,10 +32,6 @@ struct Vector2 {
     Vector2(): x(0.f), y(0.f) {}
     Vector2(double i, double j): x(i), y(j) {}
     Vector2(int i, int j): x(static_cast<double>(i)), y(static_cast<double>(j)) {}
-    Vector2(double magnitude, double x_angle, bool sad = true){
-        x = magnitude * std::cos(x_angle);
-        y = magnitude * std::sin(x_angle);
-    }
 };
 
 // Enums to store parameter name and table
@@ -57,15 +53,15 @@ struct ParameterInfo{
 
 // Map to store user input and values to be displayed to the user
 std::map<Parameter, ParameterInfo> projectile_parameters {
-    {Parameter::V_INITIAL_I_COMPONENT, ParameterInfo{"v_initial_i_component", 0.f, 0.f, 1, 1000, true, ParameterTable::KINEMATICS_VECTOR, {Parameter::V_INITIAL_J_COMPONENT}}},
-    {Parameter::V_INITIAL_J_COMPONENT, ParameterInfo{"v_ininitial_j_component", 0.f, 0.f, 1, 1000, true, ParameterTable::KINEMATICS_VECTOR, {Parameter::V_INITIAL_I_COMPONENT}}},
+    {Parameter::V_INITIAL_I_COMPONENT, ParameterInfo{"v_initial_i_component", 2.f, 0.f, 1, 1000, true, ParameterTable::KINEMATICS_VECTOR, {Parameter::V_INITIAL_J_COMPONENT}}},
+    {Parameter::V_INITIAL_J_COMPONENT, ParameterInfo{"v_ininitial_j_component", 2.f, 0.f, 1, 1000, true, ParameterTable::KINEMATICS_VECTOR, {Parameter::V_INITIAL_I_COMPONENT}}},
     {Parameter::V_FINAL_I_COMPONENT, ParameterInfo{"v_final_i_component", 0.f, 0.f, 1, 1000, true, ParameterTable::KINEMATICS_VECTOR, {Parameter::V_FINAL_J_COMPONENT}}},
     {Parameter::V_FINAL_J_COMPONENT, ParameterInfo{"v_final_j_component", 0.f, 0.f, 1, 1000, true, ParameterTable::KINEMATICS_VECTOR, {Parameter::V_FINAL_I_COMPONENT}}},
     {Parameter::INITIAL_SPEED, ParameterInfo{"initial_speed", 0.f, 0.f, 1, 1000, true, ParameterTable::KINEMATICS_SCALAR, {}}},
     {Parameter::FINAL_SPEED, ParameterInfo{"final_speed", 0.f, 0.f, 1, 1000, true, ParameterTable::KINEMATICS_SCALAR, {}}},
     {Parameter::Y_INITIAL, ParameterInfo{"y_initial", 0.f, 0.f, 0, 1000, false, ParameterTable::BOTH, {}}},
     {Parameter::COEFF_FRICTION, ParameterInfo{"coeff_friction", 0.f, 0.f, 1, 1, true, ParameterTable::FORCES, {Parameter::FORCE, Parameter::TIME, Parameter::MASS}}},
-    {Parameter::ACC, ParameterInfo{"acc", 0.f, 0.f, -1000, -1, true, ParameterTable::BOTH, {}}},
+    {Parameter::ACC, ParameterInfo{"acc", -2.f, 0.f, -1000, -1, true, ParameterTable::BOTH, {}}},
     {Parameter::FORCE, ParameterInfo{"force", 0.f, 0.f, 1, 1000, true, ParameterTable::FORCES, {Parameter::TIME, Parameter::MASS}}},
     {Parameter::ANGLE, ParameterInfo{"angle", 45.f, 45.f, 0, 90, false, ParameterTable::BOTH, {}}},
     {Parameter::TIME, ParameterInfo{"time", 0.f, 0.f, 1, 1000, true, ParameterTable::BOTH, {}}},
@@ -75,15 +71,6 @@ std::map<Parameter, ParameterInfo> projectile_parameters {
     {Parameter::ABS_MAX_HEIGHT, ParameterInfo{"abs_max_height", 0.f, 0.f, 1, 1000, false, ParameterTable::BOTH, {}}},
     {Parameter::TIME_OF_APEX, ParameterInfo{"apexTime", 0.f, 0.f, 1, 1000, false, ParameterTable::BOTH, {}}}
 };
-
-// Sets all the values in the map to their default values - runs when clear button is pressed or when the user switches tabs in the GUI
-void clear_userInput() {
-    for (auto &[_, info] : projectile_parameters){
-        info.value = info.default_value;
-    }
-
-    user_error_message = ""; // Clear error message string
-}
 
 // Physics Engine
 void find_unknown(double &y_initial, double &v_initial, double &v_final, double &acc, double &time, double &max_height, double &abs_max_height, double &range, const double &angle, double &v_initial_i_component, double &v_initial_j_component, double &v_final_i_component, double &v_final_j_component, double &apexTime) {    
@@ -271,12 +258,15 @@ void find_unknown(double &y_initial, double &v_initial, double &v_final, double 
 
     abs_max_height = y_initial + max_height; // Calculate maximum height (absolute) with respect to ground
     apexTime = (-1 * v_initial * std::sin(theta)) / acc / 2; // Calculate or recalculate time the projectile needs to reach maximum height with respect to launch
+    is_solved = true;
+    user_error_message = "";
 }
 
 // Verifies all input fields
 void cleanup_input(){
     unsigned int required_scalar_count {}, required_vector_count {};
     std::vector<int> given {};
+    is_solved = false;
 
     // Verify all values are inside their ranges
     for (const auto &[name, info] : projectile_parameters){
@@ -309,10 +299,10 @@ void cleanup_input(){
 
         if (info.is_required) {
             if (info.info_type == ParameterTable::KINEMATICS_SCALAR || info.info_type == ParameterTable::BOTH)
-                ++required_scalar_count;
+                required_scalar_count++;
 
             if (info.info_type == ParameterTable::KINEMATICS_VECTOR || info.info_type == ParameterTable::BOTH)
-                ++required_vector_count;
+                required_vector_count++;
         }
     }
 
@@ -361,10 +351,11 @@ void cleanup_input(){
         // Check output value -> <0 means the input values lead to an impossible case
         if(projectile_parameters[Parameter::MAX_HEIGHT].value  < 0) {
             user_error_message = "User has entered inconsistent values!\nLook at the calculated value of Maximum Height!";
+            is_solved = false;
             return;
         }
     } 
-    
+
     else {
         user_error_message = "Not enough required inputs. Required scalar count = " + std::to_string(required_scalar_count) +
                   ", required vector count = " + std::to_string(required_vector_count);
@@ -417,7 +408,7 @@ class dynamic_object_manager: public static_object_manager{
     public:
         dynamic_object_manager(){}
 
-        void move(const shape_ptr obj, const Vector2 &d_pos){
+        void move(const shape_ptr &obj, const Vector2 &d_pos){
             obj->move({d_pos.x, d_pos.y});
         }
 
@@ -426,19 +417,68 @@ class dynamic_object_manager: public static_object_manager{
 dynamic_object_manager dynamic_object_handler {};
 
 // Projectile Class
-// Config
 class projectile_manager {
     private:
-        std::unique_ptr<shape_ptr> object_ptr;
-    
+        shape_ptr object_ptr;
+        double t {0};
+        const double radius {30};
+
+    // Normalizes coords to the bottom right of the screen
+    void normalize_coords(double &y){
+        double height = static_cast<float>(window->getSize().y);
+        y = height - y;
+    }
+
     public:
+        std::vector<Vector2> pos_list;
+        const double start_x{15}, start_y{30};
+        double x, y;
+    
         projectile_manager(){
-            shape_ptr object_ptr = dynamic_object_handler.add_object(sf::CircleShape{30.f}, sf::Color::Red, Vector2(10,10));
+            this-> x = this->start_x;
+            this->y = start_y;
+            normalize_coords(this->y);
+
+            this->pos_list.push_back(Vector2(this->x, this->y));
+            auto circle = sf::CircleShape{this->radius};
+            circle.setOrigin({this->radius, this->radius});
+            this->object_ptr = dynamic_object_handler.add_object(circle, sf::Color::Red, Vector2(this->x, this->y));
         }
 
-        void move(const Vector2 &pos){
+    void move(){
+        this->t += TIME_INTERVAL;
+        double angle_rad = projectile_parameters[Parameter::ANGLE].value * (M_PI / 180);
+        double v_initial_x {}, v_initial_y {};
 
+        if (projectile_parameters[Parameter::INITIAL_SPEED].value != 0){
+            v_initial_x = projectile_parameters[Parameter::INITIAL_SPEED].value * std::cos(angle_rad);
+            v_initial_y = projectile_parameters[Parameter::INITIAL_SPEED].value * std::sin(angle_rad);
         }
+        else{
+            v_initial_x = projectile_parameters[Parameter::V_INITIAL_I_COMPONENT].value;
+            v_initial_y = projectile_parameters[Parameter::V_INITIAL_J_COMPONENT].value;
+        }
+
+        double new_x = this->start_x + (v_initial_x * this->t);
+        double new_y = this->start_y + (v_initial_y * this->t + 0.5 * projectile_parameters[Parameter::ACC].value * pow(this->t, 2));
+        normalize_coords(new_y); // this->start_y
+
+        double del_x = new_x - this->x; 
+        double del_y = new_y - this->y;
+        this->x += del_x; this->y += del_y; 
+
+        double height = static_cast<float>(window->getSize().y);
+        if (height - this->y < 0){
+            std::cout << "end" << std::endl;
+            stop_time = true;
+            this->y = this->start_y;
+            normalize_coords(this->y);
+        }
+        std::cout << del_x << " " << del_y << " " << this->y << std::endl;
+
+        dynamic_object_handler.move(this->object_ptr, Vector2(del_x, del_y));
+        this->pos_list.push_back(Vector2(this->x, this->y));// So we can let the user more back and forwards in the frame
+    }
 };
 
 // Dotted line handler
@@ -460,28 +500,33 @@ class dotted_line_manager {
         }
 };
 
+// Sets all the values in the map to their default values - runs when clear button is pressed or when the user switches tabs in the GUI
+void clear_userInput(projectile_manager &main_projectile) {
+    for (auto &[_, info] : projectile_parameters){
+        info.value = info.default_value;
+    }
+
+    user_error_message = ""; // Clear error message string
+    is_solved = false;
+    stop_time = false;
+    main_projectile.x = main_projectile.start_x;
+    main_projectile.y = main_projectile.start_y;
+    main_projectile.pos_list.clear();
+}
+
 // Handles all of the keyboard interactions
 void process_keyboard(){
-    if (ImGui::IsKeyPressed(ImGui::GetKeyIndex(ImGuiKey_W)))
-        camera.move({0.f, CAMERA_SPEED});
-
-    else if (ImGui::IsKeyPressed(ImGui::GetKeyIndex(ImGuiKey_A)))
-        camera.move({CAMERA_SPEED, 0.f});    
-
-    else if (ImGui::IsKeyPressed(ImGui::GetKeyIndex(ImGuiKey_S)))
-        camera.move({0.f, -CAMERA_SPEED});
-
-    else if (ImGui::IsKeyPressed(ImGui::GetKeyIndex(ImGuiKey_D)))
-        camera.move({-CAMERA_SPEED, 0.f});
-    
-    else if (ImGui::IsKeyPressed(ImGui::GetKeyIndex(ImGuiKey_P))) {
-
+    if (ImGui::IsKeyPressed(ImGui::GetKeyIndex(ImGuiKey_P))) {
+        stop_time = !stop_time;
+    }
+    else if (ImGui::IsKeyPressed(ImGui::GetKeyIndex(ImGuiKey_LeftArrow))) {
+    }
+    if (ImGui::IsKeyPressed(ImGui::GetKeyIndex(ImGuiKey_RightAlt))) {
     }
 }
 
-void render_gui(){
-    // Holds which tab is currently selected, used for clearing values on switch
-    static ParameterTable table_state {};
+void render_gui(projectile_manager &main_projectile){
+    static ParameterTable table_state {}; // Holds which tab is currently selected, used for clearing values on switch
 
     // --- Input Tab ---
     // Set initial position of the window
@@ -502,7 +547,7 @@ void render_gui(){
                 if (ImGui::BeginTabItem("Scalar Values")) {
                      // --- Input Fields ---
                     if(table_state != ParameterTable::KINEMATICS_SCALAR) {
-                        clear_userInput();
+                        clear_userInput(main_projectile);
                         table_state = ParameterTable::KINEMATICS_SCALAR;
                     }
 
@@ -521,7 +566,7 @@ void render_gui(){
                 if (ImGui::BeginTabItem("Vector Values")) {
                     // --- Input Fields ---
                     if(table_state != ParameterTable::KINEMATICS_VECTOR) {
-                        clear_userInput();
+                        clear_userInput(main_projectile);
                         table_state = ParameterTable::KINEMATICS_VECTOR;
                     }
 
@@ -559,7 +604,7 @@ void render_gui(){
         if (ImGui::BeginTabItem("Forces")) {
             // --- Input Fields ---
             if(table_state != ParameterTable::FORCES) {
-                clear_userInput();
+                clear_userInput(main_projectile);
                 table_state = ParameterTable::FORCES;
             }
 
@@ -587,12 +632,12 @@ void render_gui(){
         ImGui::PopStyleColor();
         ImGui::EndChild();
 
-        if(ImGui::Button("PLAY")) {
-        cleanup_input();
+        if(ImGui::Button("CALCULATE")) {
+            cleanup_input();
         }
 
-        ImGui::SameLine(50); if(ImGui::Button("CLEAR")) {
-            clear_userInput();
+        ImGui::SameLine(100); if(ImGui::Button("CLEAR")) {
+            clear_userInput(main_projectile);
         }
     }
 
@@ -613,7 +658,16 @@ void render_gui(){
     // Playback controls
     ImGui::Button("<<"); 
     ImGui::SameLine();
-    ImGui::Button("Play/Pause"); 
+    if (ImGui::Button(stop_time ? "Play" : "Pause")){
+        if (!is_solved){
+            user_error_message = "Can not play without values";
+
+        }
+        else{
+            stop_time = !stop_time;
+        }
+
+    }
     ImGui::SameLine();
     ImGui::Button(">>");
     // Create table with borders and row backgrounds
@@ -636,8 +690,16 @@ void render_gui(){
     ImGui::End();
 }
 
+// Check if the projectile left the screen
+bool is_projectile_off_screen(projectile_manager &main_projectile){
+    return (main_projectile.x + 30 < 0 ||        // left
+            main_projectile.x > WIDTH ||         // right
+            main_projectile.y + 30 < 0 ||        // top
+            main_projectile.y > HEIGHT);         // bottom
+}
+
 // Main window processing handler
-void window_processing(){
+void window_processing(projectile_manager &main_projectile){
     process_keyboard();
     static sf::Clock clock;
 
@@ -655,12 +717,21 @@ void window_processing(){
     // ImGUI Drawing
     // Update and re-draw the imGUI contents
     ImGui::SFML::Update(*window, clock.restart());
-    render_gui();
+    render_gui(main_projectile);
 
     // SFML Drawing
     // Set the user view as the camera
     window->clear(sf::Color::Black);
+    follow_projectile = is_projectile_off_screen(main_projectile);
+    if (false){//follow_projectile
+        double height = static_cast<float>(window->getSize().y);
+        camera.setCenter({main_projectile.x, height - main_projectile.y});
+    }
     window->setView(camera);
+
+    // Move
+    if (!stop_time)
+        main_projectile.move();
 
     // Draw all objects
     static_object_renderer.draw();
@@ -689,12 +760,22 @@ int main(){
         return -1;
 
     // Add objects to the frame
-    //static_object_renderer.add_object(sf::RectangleShape({200.f, 200.f}), sf::Color::Green, Vector2(10,10));
-    dynamic_object_handler.add_object(sf::CircleShape{30.f}, sf::Color::Red, Vector2(10,10));
+    projectile_manager main_projectile {};
+
+    // Draw some boxes in the bacground for scale
+    sf::Color color;
+    for (int x = 0; x < projectile_parameters[Parameter::RANGE].max * 100; x += 100) {
+        // Alternate colors: even stripes white, odd stripes black
+        if ((x/100) % 2 == 0){
+            color = sf::Color(43, 81, 134);
+            static_object_renderer.add_object(sf::RectangleShape({100.f, 1000.f}), color, Vector2(x, 0));
+        }   
+        // Dont need to draw black rectangle, background is already black so saves memory by not drawing unnecessary objects   
+    }
 
     // While the window is open, run the main processing loop
     while (window->isOpen()){
-        window_processing();
+        window_processing(main_projectile);
     }
 
     // Shutdown the ImGUI safely and end execution 
